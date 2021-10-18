e655eb2a6515d250d1a5d7f589ca06dc03d3eaad
Maftuh <maftuhin@mymindstores.com>
2021-10-18 21:32:03 +0700

    created_at

--- app/Models/News.php
+++ app/Models/News.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model{
 
     protected $hidden = [
-        'created_at','updated_at','published_at','created_by','updated_by'
+        'updated_at','published_at','created_by','updated_by'
     ];
 }
