7712cbc617ff47af18f1f61efe7b2d55eb7e8107
Maftuh <maftuhin@mymindstores.com>
2021-11-30 17:17:40 +0700

    upload image donation

--- app/Http/Controllers/AdmController.php
+++ app/Http/Controllers/AdmController.php
@@ -30,9 +30,7 @@ class AdmController extends Controller
         $image = null;
         if ($request->hasFile('photo')) {
             $photo = $request->file('photo');
-
             $storagePath = './images/donation';
-            $originalFileName = $photo->getClientOriginalName();
             $fileExtension = $photo->getClientOriginalExtension();
             $imageFileName = $validated["phone"] . '_' . Carbon::now() . '.' . $fileExtension;
 
@@ -41,19 +39,17 @@ class AdmController extends Controller
                 $image = url() . '/images/donation/' . $imageFileName;
             }
         }
-        $validated["images"] = $image;
+        
+        $insert = Donation::insert([
+            "name" => $validated["name"],
+            "address" => $validated["address"],
+            "phone" => $validated["phone"],
+            "email" => $validated["email"],
+            "donation" => $validated["donation"],
+            "image" => $image,
+        ]);
 
-        return $validated;
-        // $insert = Donation::insert([
-        //     "name" => $validated["name"],
-        //     "address" => $validated["address"],
-        //     "phone" => $validated["phone"],
-        //     "email" => $validated["email"],
-        //     "donation" => $validated["donation"],
-        //     "image" => $image,
-        // ]);
-
-        // return $this->actionValidation($insert, "Success");
+        return $this->actionValidation($insert, "Success");
     }
 
     //Suggest
